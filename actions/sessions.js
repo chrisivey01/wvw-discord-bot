847396b5341cf5da627b252ae77587ff3de3d318
Chris Ivey <chrisivey01@gmail.com>
2019-04-03 22:00:03 -0500

    update

--- actions/sessions.js
+++ actions/sessions.js
@@ -15,37 +15,41 @@ module.exports = {
 
         //need to obtain newSessions off gw2 api
         message.channel.send('Processing...')
-        let getKillResponse = await services.obtainAchievements(queryUserResults[0].api)
-        await delay(1000)
-        let killAchievements = getKillResponse.data.find(res => res.id === 283);
-        newSessionKills = killAchievements.current
+        try {
+            let getKillResponse = await services.obtainAchievements(queryUserResults[0].api)
+            await delay(1000)
+            let killAchievements = getKillResponse.data.find(res => res.id === 283);
+            newSessionKills = killAchievements.current
 
-        let obtainedKills = newSessionKills - lastSessionKills 
+            let obtainedKills = newSessionKills - lastSessionKills
 
-        //calculate time
-        let dbTime = new Date(queryUserResults[0].time_stamp)
-        let currentTime = new Date();
+            //calculate time
+            let dbTime = new Date(queryUserResults[0].time_stamp)
+            let currentTime = new Date();
 
-        //in ms
-        let msHours =  currentTime.getTime() - dbTime.getTime();
-        let answer = destructMS(msHours)
-        // answer = {d: 0, h: 0, m: 10, s: 54, ms: 263}
+            //in ms
+            let msHours = currentTime.getTime() - dbTime.getTime();
+            let answer = destructMS(msHours)
+            // answer = {d: 0, h: 0, m: 10, s: 54, ms: 263}
 
 
+            message.channel.send("Your last session kills are " + lastSessionKills + ". Your new session is  " + newSessionKills + ". A total of " + obtainedKills + " obtained." +
+                "Within the last " + answer.d + " days, " + answer.h + " hours, " + answer.m + " minutes, " + answer.s + " seconds since you've updated!")
 
-        message.channel.send("Your last session kills are " + lastSessionKills + ". Your new session is  " + newSessionKills + ". A total of " + obtainedKills + " obtained." +
-            "Within the last " + answer.d + " days, " + answer.h + " hours, " + answer.m+ " minutes, " + answer.s + " seconds since you've updated!")
+            // "UPDATE users SET weekly_tally = ?, weekly_kill_total = ? WHERE api = ?";
 
-        // "UPDATE users SET weekly_tally = ?, weekly_kill_total = ? WHERE api = ?";
-
-        let updateKills = 'UPDATE account_info SET currentKills = ?, time_stamp = ? WHERE uid = ? and api = ?'
-        let sqlObj = [
-            newSessionKills,
-            currentTime,
-            message.author.id,
-            queryUserResults[0].api
-        ]
-        await pool.query(updateKills, sqlObj)
+            let updateKills = 'UPDATE account_info SET currentKills = ?, time_stamp = ? WHERE uid = ? and api = ?'
+            let sqlObj = [
+                newSessionKills,
+                currentTime,
+                message.author.id,
+                queryUserResults[0].api
+            ]
+            await pool.query(updateKills, sqlObj)
+        }catch(e){
+            console.log(e.response)
+            message.channel.send('You are not verified - please kill yourself instead.')
+        }
     }
 }
 
