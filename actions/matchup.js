640d816485217175f8b8b308160f7eedb94c4f14
Chris Ivey <chrisivey01@gmail.com>
2019-04-12 08:56:37 -0500

    update

--- actions/matchup.js
+++ actions/matchup.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-
 const tier1Api = "https://api.guildwars2.com/v2/wvw/matches/1-1";
 const tier2Api = "https://api.guildwars2.com/v2/wvw/matches/1-2";
 const tier3Api = "https://api.guildwars2.com/v2/wvw/matches/1-3";
@@ -16,52 +15,81 @@ module.exports =  {
     matchUp: async (message,client) => {
         if(message.channel.id === tier1ChannelId) {
             let response = await axios(tier1Api);
+            let getBlue = response.data.worlds.blue;
+            let getRed = response.data.worlds.red;
+            let getGreen = response.data.worlds.green;
 
-            message.channel.send("Green score: " + response.data.scores.green + "\n" +
-                "Blue score: " + response.data.scores.blue + "\n" +
-                "Red score: " + response.data.scores.red + "\n" +
-                "Green kills: " + response.data.kills.green + "\n" +
-                "Blue kills: " + response.data.kills.blue + "\n" +
-                "Red kills: " + response.data.kills.red + "\n" +
-                "Green deaths: " + response.data.deaths.green + "\n" +
-                "Blue deaths: " + response.data.deaths.blue + "\n" +
-                "Red deaths: " + response.data.deaths.red + "\n");
+            let blueServer = worldArray.filter(blue => getBlue === blue.id);
+            let redServer = worldArray.filter(red => getRed === red.id);
+            let greenServer = worldArray.filter(green => getGreen === green.id);
+
+            message.channel.send(greenServer.name + " score: " + response.data.scores.green + "\n" +
+                blueServer.name + " score: " + response.data.scores.blue + "\n" +
+                redServer.name + " score: " + response.data.scores.red + "\n" +
+                greenServer.name + " kills: " + response.data.kills.green + "\n" +
+                blueServer.name + " kills: " + response.data.kills.blue + "\n" +
+                redServer.name + " kills: " + response.data.kills.red + "\n" +
+                greenServer.name + " deaths: " + response.data.deaths.green + "\n" +
+                blueServer.name + " deaths: " + response.data.deaths.blue + "\n" +
+                blueServer.name + " deaths: " + response.data.deaths.red + "\n");
         }else if(message.channel.id === tier2ChannelId){
             let response = await axios(tier2Api);
+            let getBlue = response.data.worlds.blue;
+            let getRed = response.data.worlds.red;
+            let getGreen = response.data.worlds.green;
 
-            message.channel.send("Green score: " + response.data.scores.green + "\n" +
-                "Blue score: " + response.data.scores.blue + "\n" +
-                "Red score: " + response.data.scores.red + "\n" +
-                "Green kills: " + response.data.kills.green + "\n" +
-                "Blue kills: " + response.data.kills.blue + "\n" +
-                "Red kills: " + response.data.kills.red + "\n" +
-                "Green deaths: " + response.data.deaths.green + "\n" +
-                "Blue deaths: " + response.data.deaths.blue + "\n" +
-                "Red deaths: " + response.data.deaths.red + "\n");
+            let blueServer = worldArray.filter(blue => getBlue === blue.id);
+            let redServer = worldArray.filter(red => getRed === red.id);
+            let greenServer = worldArray.filter(green => getGreen === green.id);
+
+            message.channel.send(greenServer.name + " score: " + response.data.scores.green + "\n" +
+                blueServer.name + " score: " + response.data.scores.blue + "\n" +
+                redServer.name + " score: " + response.data.scores.red + "\n" +
+                greenServer.name + " kills: " + response.data.kills.green + "\n" +
+                blueServer.name + " kills: " + response.data.kills.blue + "\n" +
+                redServer.name + " kills: " + response.data.kills.red + "\n" +
+                greenServer.name + " deaths: " + response.data.deaths.green + "\n" +
+                blueServer.name + " deaths: " + response.data.deaths.blue + "\n" +
+                blueServer.name + " deaths: " + response.data.deaths.red + "\n");
+
         }else if(message.channel.id === tier3ChannelId){
             let response = await axios(tier3Api);
+            let getBlue = response.data.worlds.blue;
+            let getRed = response.data.worlds.red;
+            let getGreen = response.data.worlds.green;
 
-            message.channel.send("Green score: " + response.data.scores.green + "\n" +
-                "Blue score: " + response.data.scores.blue + "\n" +
-                "Red score: " + response.data.scores.red + "\n" +
-                "Green kills: " + response.data.kills.green + "\n" +
-                "Blue kills: " + response.data.kills.blue + "\n" +
-                "Red kills: " + response.data.kills.red + "\n" +
-                "Green deaths: " + response.data.deaths.green + "\n" +
-                "Blue deaths: " + response.data.deaths.blue + "\n" +
-                "Red deaths: " + response.data.deaths.red + "\n");
+            let blueServer = worldArray.filter(blue => getBlue === blue.id);
+            let redServer = worldArray.filter(red => getRed === red.id);
+            let greenServer = worldArray.filter(green => getGreen === green.id);
+
+            message.channel.send(greenServer.name + " score: " + response.data.scores.green + "\n" +
+                blueServer.name + " score: " + response.data.scores.blue + "\n" +
+                redServer.name + " score: " + response.data.scores.red + "\n" +
+                greenServer.name + " kills: " + response.data.kills.green + "\n" +
+                blueServer.name + " kills: " + response.data.kills.blue + "\n" +
+                redServer.name + " kills: " + response.data.kills.red + "\n" +
+                greenServer.name + " deaths: " + response.data.deaths.green + "\n" +
+                blueServer.name + " deaths: " + response.data.deaths.blue + "\n" +
+                blueServer.name + " deaths: " + response.data.deaths.red + "\n");
         }else if(message.channel.id === tier4ChannelId){
             let response = await axios(tier4Api);
+            let getBlue = response.data.worlds.blue;
+            let getRed = response.data.worlds.red;
+            let getGreen = response.data.worlds.green;
 
-            message.channel.send("Green score: " + response.data.scores.green + "\n" +
-                "Blue score: " + response.data.scores.blue + "\n" +
-                "Red score: " + response.data.scores.red + "\n" +
-                "Green kills: " + response.data.kills.green + "\n" +
-                "Blue kills: " + response.data.kills.blue + "\n" +
-                "Red kills: " + response.data.kills.red + "\n" +
-                "Green deaths: " + response.data.deaths.green + "\n" +
-                "Blue deaths: " + response.data.deaths.blue + "\n" +
-                "Red deaths: " + response.data.deaths.red + "\n");
+            let blueServer = worldArray.filter(blue => getBlue === blue.id);
+            let redServer = worldArray.filter(red => getRed === red.id);
+            let greenServer = worldArray.filter(green => getGreen === green.id);
+
+            message.channel.send(greenServer.name + " score: " + response.data.scores.green + "\n" +
+                blueServer.name + " score: " + response.data.scores.blue + "\n" +
+                redServer.name + " score: " + response.data.scores.red + "\n" +
+                greenServer.name + " kills: " + response.data.kills.green + "\n" +
+                blueServer.name + " kills: " + response.data.kills.blue + "\n" +
+                redServer.name + " kills: " + response.data.kills.red + "\n" +
+                greenServer.name + " deaths: " + response.data.deaths.green + "\n" +
+                blueServer.name + " deaths: " + response.data.deaths.blue + "\n" +
+                blueServer.name + " deaths: " + response.data.deaths.red + "\n");
         }else{
             message.channel.send("This only works in match up channels.")
         }
